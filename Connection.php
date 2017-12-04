b8c5fc61c89b09bb045994a02735142458dd1970
salwazh1412 <salwa.zh@hotmail.com>
2017-12-05 00:05:14 +0300

    s

--- Connection.php
+++ Connection.php
@@ -18,11 +18,16 @@ $SqlEncoding=mysql_client_encoding($database); */
 $servername = "localhost";
 $username = "root";
 $password = "";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=mykidney", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=mykidney", $username, $password,$opt);
     // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 catch(PDOException $e)
     {
