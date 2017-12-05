76fd53df51dbbb20a3fc114844ed06e89f726e1f
salwazh1412 <salwa.zh@hotmail.com>
2017-12-05 06:49:22 +0300

    Manage Requests

---
+++ ManageRequests.php
@@ -0,0 +1,223 @@
+<!DOCTYPE HTML>
+
+ <?php require ('connection.php'); ?>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>myKidney</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+	<nav class="gtco-nav" role="navigation">
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-xs-2">
+					<!-- <div id="gtco-logo"><a href="index.php">MyKidney</a></div> -->
+                    <div id="gtco-logo"><a href="index.php"><img src="images/Logo.png" style="width:170px;"></a></div>
+				</div>
+				<div class="col-xs-8 text-center menu-1">
+					<br>
+                    <ul>
+						<li class="active"><a href="AdminHome.php">Home</a></li>
+				        <!-- <li><a href="">Search</a></li>
+                        <li><a href="about.php">About</a></li>
+						<li><a href="contact.php">Contact</a></li> -->
+					</ul>
+				</div>
+				<div class="col-xs-2 text-right hidden-xs menu-2">
+					
+                    <ul>
+                        <br><li class="btn-cta2"><a href="Login.php"><span> <div align="center">Logout</div>   </span></a></li>
+					</ul>
+					<ul>
+                        <!-- <li class="btn-cta"><a href="#Signup"><span>Sign Up</span></a></li> -->
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="gtco-header" class="gtco-cover" role="banner" style="background-image:url(images/img_bg_1.jpg); height:170px;">
+
+	</header>
+        
+<style>
+table, th, td {
+    border: 1px solid gray;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+th {
+    text-align: center;
+}
+</style>
+        
+        
+	<div id="gtco-features">
+		<div class="gtco-container">
+			<div class="row" Style="margin-top:-150px;">
+                
+				<div>
+                    <div class="feature-center animate-box" data-animate-effect="fadeIn">
+                           
+                        
+	<div id="gtco-services">
+		<div class="gtco-container">
+			
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+					<h2>Manage Requests</h2>
+				<!--	<p>Select user to delete</p> -->
+				</div>
+			</div>
+ <!-- 
+			<div class="row animate-box">
+				
+				<div class="gtco-tabs">
+					<ul class="gtco-tab-nav">
+						<li class="active"><a href="#" data-tab="1"><span class="icon visible-xs"><i class="icon-command"></i></span>
+                            <span class="hidden-xs">Web Design</span></a></li>
+						<li><a href="#" data-tab="2"><span class="icon visible-xs"><i class="icon-bar-graph"></i></span>
+                            <span class="hidden-xs">Online Marketing</span></a></li>
+						<li><a href="#" data-tab="3">
+                            <span class="icon visible-xs"><i class="icon-bag"></i></span><span class="hidden-xs">E-Commerce</span></a></li>
+					</ul>
+
+					
+					<div class="gtco-tab-content-wrap">
+						
+                    
+                        
+                      <div class="gtco-tab-content tab-content active" data-tab-content="1">
+							<div class="col-md-12">
+								<h2>Web Design</h2>
+								<p>Paragraph placeat quis fugiat provident veritatis quia iure a debitis adipisci dignissimos consectetur magni quas eius nobis reprehenderit soluta eligendi quo reiciendis fugit? Veritatis tenetur odio delectus quibusdam officiis est.</p>
+
+								<p>Ullam dolorum iure dolore dicta fuga ipsa velit veritatis molestias totam fugiat soluta accusantium omnis quod similique placeat at. Dolorum ducimus libero fuga molestiae asperiores obcaecati corporis sint illo facilis.</p>
+
+							</div>
+						</div>
+                        
+                        
+                     
+                        
+                        <div class="gtco-tab-content tab-content active" data-tab-content="2">
+							<div class="col-md-12">
+								<h2>Web Design</h2>
+								<p>Paragraph placeat quis fugiat provident veritatis quia iure a debitis adipisci dignissimos consectetur magni quas eius nobis reprehenderit soluta eligendi quo reiciendis fugit? Veritatis tenetur odio delectus quibusdam officiis est.</p>
+
+								<p>Ullam dolorum iure dolore dicta fuga ipsa velit veritatis molestias totam fugiat soluta accusantium omnis quod similique placeat at. Dolorum ducimus libero fuga molestiae asperiores obcaecati corporis sint illo facilis.</p>
+
+							</div>
+						</div>
+                        
+                          
+                        
+                        <div class="gtco-tab-content tab-content active" data-tab-content="3">
+							<div class="col-md-12">
+								<h2>Web Design</h2>
+								<p>Paragraph placeat quis fugiat provident veritatis quia iure a debitis adipisci dignissimos consectetur magni quas eius nobis reprehenderit soluta eligendi quo reiciendis fugit? Veritatis tenetur odio delectus quibusdam officiis est.</p>
+
+								<p>Ullam dolorum iure dolore dicta fuga ipsa velit veritatis molestias totam fugiat soluta accusantium omnis quod similique placeat at. Dolorum ducimus libero fuga molestiae asperiores obcaecati corporis sint illo facilis.</p>
+
+							</div>
+						</div> 
+					</div>
+
+				</div>
+			</div> -->
+		</div>
+	</div>
+                        <?php   
+                        $target='./Tests/';
+                        echo "<table style='width:100%';>";
+                        echo "<tr><th>Patient Name</th><th>Patient Tests</th><th>Donor Name</th><th>Donor Tests</th><th>Mach</th></tr>";
+                        try {
+                            $stmt = $conn->prepare("SELECT patient.Name as PName, patient.Test as PTest, donor.Name as DName, donor.Test as DTest 
+                                FROM requests 
+                                INNER JOIN patient ON patient.ID=requests.Patients_ID
+                                INNER JOIN donor ON donor.ID=requests.Donor_ID
+                                where requests.Donor_Approval=1 ORDER BY requests.Request_Date;");
+                            $stmt->execute();
+                            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);                            
+                                foreach ($stmt as $row){
+                                    echo "<tr>"."<td>".$row['PName']."</td>".
+                                        "<td>". "<a href=" .$target.basename($row['PTest']).">Download</a>"."</td>".
+                                        "<td>".$row['DName']."</td>".
+                                        "<td>". "<a href=" .$target.basename($row['DTest']).">Download</a>"."</td>".
+                                        "<td>". "" ."</td>".           
+                                        "</tr>"."\n";
+                                }
+                        }catch(PDOException $e) {
+                         echo "Error: " . $e->getMessage();
+                        }
+                        echo "</table>";
+
+                        ?>
+
+                    
+                        
+                    </div> 
+                </div> 
+
+                
+			</div> <!-- end of row div -->
+		</div>
+	</div>
+
+
+	
+	<!------ Footer -------->		
+<?php
+        $conn = null;
+include ('footer.html');
+?>
